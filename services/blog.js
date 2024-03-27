dd8d42277f9dc877010599cf9477936d70c495fd
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-03-27 11:17:47 -0600

    cambio en el servicio de blog

--- services/blog.js
+++ services/blog.js
@@ -1,8 +1,6 @@
 import axios from "axios"
 
 export const getAllData = async()=>{
-    const urls = []
-    const response = await axios.get("https://www.googleapis.com/blogger/v3/blogs/6035337752081805288/posts?key=AIzaSyCUbCHfwnO1P8oRvK1iHASDBaByyvPuEmE")
-    response.data.items.forEach((i)=> urls.push({url: i.url, title: i.title}))
-    return urls
+    const response = await axios.get("https://cdserver-r54a.vercel.app/blog")
+    return response.data
 }
