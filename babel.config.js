ebd7e621ad04733728d174f06af3a31957bd3f7c
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-04-12 11:54:53 -0600

    correccion de cierre de aplicacion

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["babel-plugin-transform-html-import-to-string"],
+    plugins: ["babel-plugin-transform-html-import-to-string", "nativewind/babel"],
   };
 };
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -5,24 +5,48 @@ import { getAllData } from "../services/blog";
 import Loader from "../components/Loader";
 import BlogPost from "../components/BlogPost";
 import Slider from "../components/Slider";
-import { BannerAd } from 'react-native-google-mobile-ads';
+import {AdManager} from 'react-native-admob-native-ads';
+import { AdView } from "../components/AdView";
+
 
 const Home = ({ navigation })=>{
+    ////
+    const [currentRoute, setCurrentRoute] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const init = async () => {
+  
+  
+        // await AdManager.setRequestConfiguration({
+        //   trackingAuthorized: true,
+        // });
+  
+        setLoading(false);
+      };
+  
+      init();
+    }, []);
+    /////
     const [posts, setPosts] = useState()
     const data = async()=>{
        const list=  await getAllData()
        setPosts(list)
     }
+
     useEffect(()=>{
         data()
+
     },[])
+    // console.log(TestIds.BANNER)
     return(
         <View style={styles.container}>
             <Slider navigation={navigation}/>
             <ScrollView >
             {posts && posts.length >0 && posts.map((item, index )=> <BlogPost url={item.url} title={item.title} ir={navigation} key={item.url} index={index}/>)  }            
             </ScrollView>
-            <BannerAd unitId="ca-app-pub-5314398130823639~6785887471" />
+            {/* <AdView  type="image" media={false} /> */}
+            {/* <BannerAd unitId="ca-app-pub-5314398130823639~6785887471" /> */}
 {/* 
             <View style={styles.addButon} >
             <MaterialIcons name="add-to-photos" size={40} color="#365486" onPress={()=> console.log("lalalal")}/>
