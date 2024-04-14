2abc0ae72fb7f615f1ad00f7346431d29d46111a
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-04-13 18:45:49 -0600

    correccion de crasheo de primera carga, se actualizaron las librerias

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,33 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Button, FlatList, ScrollView, StatusBar } from "react-native";
-import { MaterialIcons } from '@expo/vector-icons';
 import { getAllData } from "../services/blog";
-import Loader from "../components/Loader";
 import BlogPost from "../components/BlogPost";
 import Slider from "../components/Slider";
-import {AdManager} from 'react-native-admob-native-ads';
-import { AdView } from "../components/AdView";
 
 
 const Home = ({ navigation })=>{
-    ////
-    const [currentRoute, setCurrentRoute] = useState(null);
-    const [loading, setLoading] = useState(true);
   
-    useEffect(() => {
-      const init = async () => {
-  
-  
-        // await AdManager.setRequestConfiguration({
-        //   trackingAuthorized: true,
-        // });
-  
-        setLoading(false);
-      };
-  
-      init();
-    }, []);
-    /////
     const [posts, setPosts] = useState()
     const data = async()=>{
        const list=  await getAllData()
@@ -38,19 +17,12 @@ const Home = ({ navigation })=>{
         data()
 
     },[])
-    // console.log(TestIds.BANNER)
     return(
         <View style={styles.container}>
             <Slider navigation={navigation}/>
             <ScrollView >
             {posts && posts.length >0 && posts.map((item, index )=> <BlogPost url={item.url} title={item.title} ir={navigation} key={item.url} index={index}/>)  }            
             </ScrollView>
-            {/* <AdView  type="image" media={false} /> */}
-            {/* <BannerAd unitId="ca-app-pub-5314398130823639~6785887471" /> */}
-{/* 
-            <View style={styles.addButon} >
-            <MaterialIcons name="add-to-photos" size={40} color="#365486" onPress={()=> console.log("lalalal")}/>
-            </View> */}
         </View>
     )
 }
